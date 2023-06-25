1321e2c279f7436dfd3807b69c1ce132506c4bd7
caoboyeehaw <dylanhoangcao@gmail.com>
2023-06-25 18:07:27 -0500

    good push

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,20 +2,22 @@ import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    <nav>
-      <div>
-        <Link href="/">Home</Link>
-      </div>
-      <div>
-        <Link href="/signin">Login</Link>
-      </div>
-      <style jsx>{`
-        nav {
-          display: flex;
-          justify-content: space-between;
-        }
-      `}</style>
-    </nav>
+    <div className="container mx-auto">
+      <nav className="flex justify-between items-center">
+        <div>
+          <Link href="/" className="font-bold text-2xl text-green-700">FuelPriceInsights</Link>
+        </div>
+        <div className="flex space-x-4 items-center">
+          <Link className="text-md rounded-lg px-4 py-2 text-slate-500 hover:text-black" href="/">About</Link>
+          <Link className="text-md flex items-center shadow-lg rounded-lg hover:shadow-none px-4 py-1 bg-gray-900 text-white" href="/login">
+            <span className="mx-auto">Log In</span>
+          </Link>
+          <Link className="text-md flex items-center shadow-lg rounded-lg hover:shadow-none px-4 py-1 bg-green-600 text-white" href="/signup">
+            <span className="mx-auto">Sign Up</span>
+          </Link>
+        </div>
+      </nav>
+    </div>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,13 @@ import Navbar from '../components/Navbar';
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div className="flex flex-col min-h-screen py-6 "> 
       <Navbar />
-      <h1 >This is a test for group 15</h1>
+        <div className="mt-4 border-b-2 border-gray-200">
+        </div>
+        <div className="container mx-auto py-6">
+          This is a test for group 15
+        </div>
     </div>
   );
 };
