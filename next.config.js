a40b55a37e7432d1c4c9f901e09eaf54d24553b9
caoboyeehaw <dylanhoangcao@gmail.com>
2023-06-25 17:35:41 -0500

    style changes

--- next.config.js
+++ next.config.js
@@ -2,3 +2,16 @@
 const nextConfig = {}
 
 module.exports = nextConfig
+
+
+
+
+/* @type {import('next').NextConfig} 
+module.exports = {
+    future: {
+      // Disable the profiler
+      profiler: false,
+    },
+    // your other configurations go here
+  };
+ */
