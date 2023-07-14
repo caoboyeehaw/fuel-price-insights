50fc6db779538b2a39157b5a12395ce492cd500a
bahnaynays <60674300+bahnaynays@users.noreply.github.com>
2023-07-13 21:28:40 -0500

    Implemented the backend validations in fuelquote.js and updated the corresponding typescript

---
+++ src/pages/api/fuelquote.js
@@ -0,0 +1,26 @@
+
+
+export default function handler(req, res){
+    if(req.method === 'POST'){
+        const {GallonNeeded, deliveryAddress, deliveryDate, ppg, totalAmountDue} = req.body;
+
+        //validations below
+
+        if(GallonNeeded <= 0){
+            return res.status(400).json({error: 'Gallons requested must be greater than 0'});
+        }
+
+        //validate date (setting a minimum date limit)
+        const parsedDeliveryDate = new Date(deliveryDate);
+
+        if(isNaN(parsedDeliveryDate)){
+            return res.status(400).json({error: 'Must select a valid delivery date'});
+        }
+
+        const currentDate = new Date();
+        
+        if(parsedDeliveryDate < currentDate){
+            return res.status(400).json({error: 'Delivery date cannot be set in the past'});
+        }
+    }
+}
