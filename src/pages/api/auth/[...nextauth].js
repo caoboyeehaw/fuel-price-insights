4f3ebeb607a34eba086a5de0f220790683d6c936
bahnaynays <60674300+bahnaynays@users.noreply.github.com>
2023-07-20 18:47:45 -0500

    incomplete nextauth js page.

---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,17 @@
+/*import NextAuth from 'next-auth'
+import Providers from 'next-auth/providers'
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
+import {MongoClient } from "mongodb"
+
+const client = new MongoClient(process.env.MONGODB_URI)
+
+export default NextAuth({
+    providers: [
+        Provers.Credentials({
+            name: 'Credentials',
+        })
+    ]
+})
+*/
+
+//THIS IS INCOMPLETE
