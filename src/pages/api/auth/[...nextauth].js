ab2c01bbcc3c58efea56521f7d2bc53e26440094
bahnaynays <60674300+bahnaynays@users.noreply.github.com>
2023-07-20 19:41:14 -0500

    incomplete next_auth

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -9,9 +9,19 @@ export default NextAuth({
     providers: [
         Provers.Credentials({
             name: 'Credentials',
+            credentials: {
+                email: { label: "Email", type: "text" },
+                password: { label: "Password", type: "password" },
+            },
+            async authorize(credentials){
+                const user = { id: 1, name: }
+            }
         })
     ]
 })
-*/
 
+//callbacks go here 
+//defines who is a customer vs admin with the use of tokens too.
+
+*/
 //THIS IS INCOMPLETE
